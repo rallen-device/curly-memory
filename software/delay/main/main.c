99cf8482fabf0c3e33cd035975914604df5b85b8
Richard Allen <richardallen@richards-mbp.home>
2021-12-25 18:51:21 +0000

    update to delay software, remember to turn off wdt in menuconfig

--- software/delay/main/main.c
+++ software/delay/main/main.c
@@ -9,17 +9,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
 #include "driver/spi_master.h"
 #include "sdkconfig.h"
 #include "esp_log.h"
 
-/* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO 32
 
 #define MISO   12
 #define MOSI   13
@@ -32,24 +25,21 @@
 #define A_CLK    18
 #define ADC_CS 5
 #define A_SPI_HOST  VSPI_HOST
-#define CLK_SPEED   10*1000*1000
+#define CLK_SPEED   1*1000*1000
 #define H_SPI_HOST    HSPI_HOST
 
 #define TRANSFER_SIZE 16
 
 static const char TAG[] = "CMD"; // curly-memory-delay tag
 
-const int wiper0writeAddr = 0b00000000;
-const int wiper1writeAddr = 0b00010000;
-const int tconwriteAddr = 0b01000000;
-const int tcon_0off_1on = 0b11110000;
-const int tcon_0on_1off = 0b00001111;
-const int tcon_0off_1off = 0b00000000;
-const int tcon_0on_1on = 0b11111111;
-
-spi_device_handle_t spi_res1;
-spi_device_handle_t spi_res2;
+spi_device_handle_t spi_dac;
 spi_device_handle_t spi_adc;
+
+#define BUF_SIZE  sizeof(uint16_t)
+
+uint16_t buf[BUF_SIZE];
+uint16_t buf_head;
+uint16_t buf_tail;
 
 /*
 * Inits the spi line
@@ -72,7 +62,7 @@ void init_spi()
 
 
   ESP_LOGI(TAG, "Initing the resistor1 spi device");
-  spi_device_interface_config_t res1_dev={
+  spi_device_interface_config_t dac_dev={
     .clock_speed_hz=CLK_SPEED,           //Clock out at 10 MHz
     .mode=0,                                //SPI mode 0
     .spics_io_num=RES_CS,               //CS pin
@@ -80,22 +70,8 @@ void init_spi()
   };
   ESP_ERROR_CHECK(ret);
   //Attach the LCD to the SPI bus
-  ret=spi_bus_add_device(H_SPI_HOST, &res1_dev, &spi_res1);
+  ret=spi_bus_add_device(H_SPI_HOST, &dac_dev, &spi_dac);
   ESP_ERROR_CHECK(ret);
-
-
-  ESP_LOGI(TAG, "Initing the resistor2 spi device");
-  spi_device_interface_config_t res2_dev={
-    .clock_speed_hz=CLK_SPEED,           //Clock out at 10 MHz
-    .mode=0,                                //SPI mode 0
-    .spics_io_num=RES2_CS,               //CS pin
-    .queue_size=7,                          //We want to be able to queue 7 transactions at a time
-  };
-  ESP_ERROR_CHECK(ret);
-  //Attach the LCD to the SPI bus
-  ret=spi_bus_add_device(H_SPI_HOST, &res2_dev, &spi_res2);
-  ESP_ERROR_CHECK(ret);
-
 
   ESP_LOGI(TAG, "Setting up SPI3 (VSPI)");
   spi_bus_config_t a_buscfg={
@@ -115,7 +91,7 @@ void init_spi()
 
   spi_device_interface_config_t adc_dev={
     .clock_speed_hz=CLK_SPEED,           //Clock out at 10 MHz
-    .mode=0,                                //SPI mode 0
+    .mode=1,                                //SPI mode 0
     .spics_io_num=ADC_CS,               //CS pin
     .queue_size=7,                          //We want to be able to queue 7 transactions at a time
   };
@@ -139,56 +115,87 @@ void send_spi(spi_device_handle_t device, uint8_t cmd, uint8_t value)
 
 }
 
-void read_adc(uint8_t single_diff, uint8_t channel)
+uint16_t read_adc(uint8_t single_diff, uint8_t channel)
 {
   static spi_transaction_t trans;
-  memset(&trans, 0, sizeof(spi_transaction_t));
-  trans.length = 3;
+//   memset(&trans, 0, sizeof(spi_transaction_t));
+  trans.length = 24;
   trans.flags = SPI_TRANS_USE_TXDATA | SPI_TRANS_USE_RXDATA;
   trans.tx_data[0] = 1;
   trans.tx_data[1] = (single_diff << 7) | (channel << 4);
   trans.tx_data[3] = 0;
+//   ESP_LOGI(TAG, "Sending value to spi : %d %d %d %d", trans.tx_data[0], trans.tx_data[1], trans.tx_data[2], trans.tx_data[3]);
   spi_device_transmit(spi_adc, &trans);
-  uint8_t adc = ((trans.rx_data[1] & 3) << 8) + trans.rx_data[2];
-  ESP_LOGI(TAG, "Sending value to spi : %d - %d %d %d %d %d", adc, trans.rxlength, trans.rx_data[0], trans.rx_data[1], trans.rx_data[2], trans.rx_data[3]);
+  uint16_t adc = ((trans.rx_data[1] & 3) << 8) + trans.rx_data[2];
+//   ESP_LOGI(TAG, "Receiving value from spi : %d - %d %d %d %d", adc, trans.rx_data[0], trans.rx_data[1], trans.rx_data[2], trans.rx_data[3]);
+  return adc;
+}
+
+void send_dac(uint8_t A_B, uint8_t gain, uint8_t shdn, uint16_t data)
+{
+//   ESP_LOGI(TAG, "Sending value to spi : %d", data);
+  static spi_transaction_t trans;
+//   memset(&trans, 0, sizeof(spi_transaction_t));
+  trans.length = 16;
+  trans.flags = SPI_TRANS_USE_TXDATA;
+  trans.tx_data[0] = (A_B << 7) | (gain << 5) | (shdn << 4) | (data >> 4);
+  trans.tx_data[1] = data << 4;
+  spi_device_transmit(spi_dac, &trans);
+//   ESP_LOGI(TAG, "Rx value from spi : %d %X %X", trans.rxlength, trans.tx_data[0], trans.tx_data[1]);
+
+}
+
+void buf_add(uint16_t value)
+{
+  buf[buf_head++] = value;
+  if (buf_head == BUF_SIZE)
+  {
+    buf_head = 0;
+  }
+}
+
+uint16_t buf_pop()
+{
+  uint16_t value = buf[buf_tail++];
+  if (buf_tail == BUF_SIZE)
+  {
+    buf_tail = 0;
+  }
+  return value;
+}
+
+
+void buf_add_delay(uint16_t value, uint16_t delay, uint8_t decay)
+{
+  uint16_t delay_value = value / decay;
+  uint16_t delay_index = buf_head - delay;
+  buf[delay_index] = delay_value;
 }
 
 
 
 void app_main(void)
 {
-  /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
-  gpio_reset_pin(BLINK_GPIO);
-  /* Set the GPIO as a push/pull output */
-  gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-
   init_spi();
 
   uint8_t count1 = 0;
-  uint8_t count2 = 0;
   uint8_t value = 0;
+
+  memset(&buf, 0, sizeof(buf));
+  buf_head = 0;
+  buf_tail = BUF_SIZE - (BUF_SIZE / 2);
 
   while(1) {
     count1++;
-    value = count1;
-    if (count1 > 127)
-    {
-      value = 127 - (count1 - 128);
-    }
-    send_spi(spi_res2, wiper0writeAddr, value);
-    if (count1 == 0)
-    {
-      vTaskDelay(1);
-    }
 
-    read_adc(1, 0);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // read value from adc as 12 bit value
+    value = read_adc(1, 0);
 
+    buf_add(value);
+    buf_add_delay(value, BUF_SIZE / 4, 1);
+
+    // send value from adc to dac as 12 bit value
+    send_dac(0, 0, 1, buf_pop());
 
   }
 }
